08afb8f9cd72fd7764f66270246e8315ef679ad6
ajafarian <a.jafarian@revotech.com.au>
2024-11-12 23:04:21 +0330

    made some small changes and added env configuration for start

--- src/models/blogModels.ts
+++ src/models/blogModels.ts
@@ -9,13 +9,13 @@ export interface BlogPost {
 export const createBlogPost = async (post: BlogPost) => {
   const { title, description, content } = post;
   const [result] = await db.query(
-    "INSERT INTO blogPosts (title, description, content) VALUES (?, ?, ?)",
+    "INSERT INTO blogposts (title, description, content) VALUES (?, ?, ?)",
     [title, description, content]
   );
   return result;
 };
 
 export const getAllBlogPosts = async () => {
-  const [rows] = await db.query("SELECT * FROM blogPosts");
+  const [rows] = await db.query("SELECT * FROM blogposts");
   return rows;
 };
